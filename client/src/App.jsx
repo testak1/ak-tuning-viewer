16f606b3b0a127c11e50321c794f9ea027fefa8e
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-17 22:26:41 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import CarsPage from './pages/CarsPage';
 import BrandPage from './pages/BrandPage';
 import ModelPage from './pages/ModelPage';
+import YearPage  from './pages/YearPage';
 import MotorPage from './pages/MotorPage';
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/cars" element={<CarsPage />} />
         <Route path="/cars/:brand" element={<BrandPage />} />
         <Route path="/cars/:brand/:model" element={<ModelPage />} />
+        <Route path="/cars/:brand/:model/:year" element={<YearPage />} />
         <Route path="/cars/:brand/:model/:year/:motor" element={<MotorPage />} />
       </Routes>
     </BrowserRouter>
