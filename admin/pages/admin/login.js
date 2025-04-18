55978bfdc25075f24f3004ea60e2c4ce22b76b30
testak1 <60112704+testak1@users.noreply.github.com>
2025-04-18 04:58:23 +0200

    Update login.js

--- admin/pages/admin/login.js
+++ admin/pages/admin/login.js
@@ -2,15 +2,33 @@ import { getCsrfToken } from 'next-auth/react';
 
 export default function Login({ csrfToken }) {
   return (
-    <form method="post" action="/api/auth/callback/credentials">
-      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-      <label>Email: <input name="email" type="email" /></label>
-      <label>Password: <input name="password" type="password" /></label>
+    <form
+      method="post"
+      action="/api/auth/callback/credentials"
+    >
+      <input name="csrfToken"   type="hidden" defaultValue={csrfToken} />
+      {/* tell NextAuth where to go after login: */}
+      <input name="callbackUrl" type="hidden" defaultValue="/admin/makes" />
+
+      <label>
+        Email:
+        <input name="email" type="email" required />
+      </label>
+
+      <label>
+        Password:
+        <input name="password" type="password" required />
+      </label>
+
       <button type="submit">Login</button>
     </form>
   );
 }
 
 export async function getServerSideProps(context) {
-  return { props: { csrfToken: await getCsrfToken(context) } };
+  return {
+    props: {
+      csrfToken: await getCsrfToken(context)
+    }
+  };
 }
